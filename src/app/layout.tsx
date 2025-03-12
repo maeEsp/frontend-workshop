cbbca5cdd6295c309d583090154551b55a79a056
maeEsp <maemae.espera@gmail.com>
2025-03-12 17:00:36 +0800

    mobile first layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+       <body>
+        <div className="container">{children}</div>
       </body>
     </html>
   );
